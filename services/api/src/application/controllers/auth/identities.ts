613e4e5e4e0c5e12ba313f0bde1a0fbafb56673b
kevinand11 <kevinfizu@gmail.com>
2022-04-16 01:54:35 +0100

    fix: expect accessToken in google auth

--- services/api/src/application/controllers/auth/identities.ts
+++ services/api/src/application/controllers/auth/identities.ts
@@ -5,9 +5,11 @@ import { generateAuthOutput } from '@utils/modules/auth'
 export class IdentitiesController {
 	static async googleSignIn (req: Request) {
 		const validatedData = validate({
+			accessToken: req.body.accessToken,
 			idToken: req.body.idToken,
 			referrer: req.body.referrer
 		}, {
+			accessToken: { required: true, rules: [Validation.isString] },
 			idToken: { required: true, rules: [Validation.isString] },
 			referrer: { required: false, rules: [Validation.isString] }
 		})
