0ee6b7d297b2ec0b2173d75be7e74b20392e80a4
kevinand11 <kevinfizu@gmail.com>
2022-09-14 00:43:11 +0100

    refactor: events

--- services/api/src/utils/jobs.ts
+++ services/api/src/utils/jobs.ts
@@ -1,17 +1,32 @@
 import { CronTypes } from '@stranerd/api-commons'
-import { EventTypes, publishers } from '@utils/events'
 import { appInstance } from '@utils/environment'
+import { EmailsUseCases } from '@modules/emails'
+import { sendMailAndCatchError } from '@utils/modules/emails'
+import { TypedEmail } from '@utils/types/email'
+import { deleteUnverifiedUsers } from '@utils/modules/auth'
 
 export const startJobs = async () => {
 	await appInstance.job.startProcessingQueues([
 		{ name: CronTypes.hourly, cron: '0 * * * *' },
-		{ name: CronTypes.daily, cron: '0 0 * * *' }
+		{ name: CronTypes.daily, cron: '0 0 * * *' },
+		{ name: CronTypes.weekly, cron: '0 0 * * SUN' },
+		{ name: CronTypes.monthly, cron: '0 0 1 * *' }
 	], {
-		onDelayed: async (data) => {
-			await publishers[EventTypes.TASKSDELAYED].publish(data)
+		onDelayed: async () => {
+		},
+		onCronLike: async () => {
 		},
 		onCron: async (type) => {
-			await publishers[EventTypes.TASKSCRON].publish({ type })
+			if (type === CronTypes.hourly) {
+				const errors = await EmailsUseCases.getAndDeleteAllErrors()
+				await Promise.all(
+					errors.map(async (error) => {
+						await sendMailAndCatchError(error as unknown as TypedEmail)
+					})
+				)
+				await appInstance.job.retryAllFailedJobs()
+			}
+			if (type === CronTypes.daily) await deleteUnverifiedUsers()
 		}
 	})
 }
