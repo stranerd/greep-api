5111eca468ffdeff5cf0d887b019d27ed141ee73
kevinand11 <kevinfizu@gmail.com>
2023-01-25 03:53:44 +0100

    fix: remove health route

--- services/api/src/application/routes/index.ts
+++ services/api/src/application/routes/index.ts
@@ -1,21 +1,8 @@
-import { makeController, Route, StatusCodes } from '@stranerd/api-commons'
-import { appId } from '@utils/environment'
+import { Route } from '@stranerd/api-commons'
 import authRoutes from './auth'
 import usersRoutes from './users'
 
 export const routes: Route[] = [
 	...authRoutes,
-	...usersRoutes,
-	{
-		path: '/',
-		method: 'get',
-		controllers: [
-			makeController(async () => {
-				return {
-					status: StatusCodes.Ok,
-					result: `${appId} service running`
-				}
-			})
-		]
-	}
+	...usersRoutes
 ]
