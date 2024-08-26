0a6a88cc6ead8c869397e70dcdd8bb4f49f5c3a3
kevinand11 <kevinfizu@gmail.com>
2024-08-26 23:59:47 +0100

    fix: can only clear non active cart

--- services/api/src/modules/marketplace/data/repositories/carts.ts
+++ services/api/src/modules/marketplace/data/repositories/carts.ts
@@ -91,7 +91,7 @@ export class CartRepository implements ICartRepository {
 	}
 
 	async clear(id: string, userId: string) {
-		const cart = await Cart.findOneAndUpdate({ _id: id, userId }, { $set: { packs: [] } }, { new: true })
+		const cart = await Cart.findOneAndUpdate({ _id: id, active: true, userId }, { $set: { packs: [] } }, { new: true })
 		return this.mapper.mapFrom(cart)
 	}
 }
